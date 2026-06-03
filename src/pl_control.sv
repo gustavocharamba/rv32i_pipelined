5c0e01e6b18cde819505dc0a0ebb203bcf4798e6
Gustavo Charamba <gucharamba@gmail.com>
2026-06-03 11:01:43 -0300

    Reconhece instruções I _TYPE, adiciona o opcode 0010011 no controle principal para reconhecer as instruções I_TYPE. Faz usar o imediato como segundo operador da ALI, escreve o resultado em rd e usa o ALUOp = 11 para decodificar no pl_alu_ctrl

--- src/pl_control.sv
+++ src/pl_control.sv
@@ -7,21 +7,22 @@
 //
 // Instrucoes suportadas:
 //   R-type  (0110011): add, and
+//   I-type  (0010011): addi, andi, ori, slti, slli, srai
 //   I-type  (0000011): lw
 //   S-type  (0100011): sw
 //   B-type  (1100011): beq
 //
 // Tabela de sinais de controle:
-//   Sinal     | R-type | lw | sw | beq
-//   ----------|--------|----|----|-----
-//   ALUSrc    |   0    |  1 |  1 |  0    0=reg, 1=imm
-//   MemtoReg  |   0    |  1 |  - |  -    0=ALU, 1=mem
-//   RegWrite  |   1    |  1 |  0 |  0
-//   MemRead   |   0    |  1 |  0 |  0
-//   MemWrite  |   0    |  0 |  1 |  0
-//   Branch    |   0    |  0 |  0 |  1
-//   ALUOp[1]  |   1    |  0 |  0 |  0
-//   ALUOp[0]  |   0    |  0 |  0 |  1
+//   Sinal     | R-type | R-type | lw | sw | beq
+//   ----------|--------|--------|----|----|-----
+//   ALUSrc    |   0    |    1   |  1 |  1 |  0    0=reg, 1=imm
+//   MemtoReg  |   0    |    0   |  1 |  - |  -    0=ALU, 1=mem
+//   RegWrite  |   1    |    1   |  1 |  0 |  0
+//   MemRead   |   0    |    0   |  1 |  0 |  0
+//   MemWrite  |   0    |    0   |  0 |  1 |  0
+//   Branch    |   0    |    0   |  0 |  0 |  1
+//   ALUOp[1]  |   1    |    1   |  0 |  0 |  0
+//   ALUOp[0]  |   0    |    1   |  0 |  0 |  1
 // =============================================================================
 
 `timescale 1ns / 1ps
@@ -38,6 +39,7 @@ module pl_control (
 );
 
     localparam R_TYPE = 7'b0110011;
+    localparam I_TYPE = 7'b0010011;
     localparam LOAD   = 7'b0000011;
     localparam STORE  = 7'b0100011;
     localparam BRANCH = 7'b1100011;
@@ -57,6 +59,12 @@ module pl_control (
                 MemtoReg = 1'b0;
                 RegWrite = 1'b1;
                 ALUOp    = 2'b10;
+            end
+            I_TYPE: begin
+                ALUSrc   = 1'b1;
+                MemtoReg = 1'b0;
+                RegWrite = 1'b1;
+                ALUOp    = 1'b11;
             end
             LOAD: begin
                 ALUSrc   = 1'b1;
